8be4521431eadf5e02896f9d1309fe39f4a06d75
Martin Johansson <maxamartin1999@gmail.com>
2020-12-10 11:07:29 +0100

    Added htmlPage functionality

--- v50uppgifter/app.js
+++ v50uppgifter/app.js
@@ -1,11 +1,22 @@
 /* Includes: */
 var http = require('http');
 
-/* Register server: */
-http.createServer(function (req, res) {
+function htmlPage(req, res) {
+    res.write('<html>');
+        res.write('<head>');
+            res.write('<title>Repeat</title>');
+        res.write('</head>');
+        res.write('<body>');
+            res.write('<h1>' + req.url + '</h1>');
+            res.write('<p>My first paragraph</p>');
+        res.write('</body>');
+    res.write('</html>');
+}
+
+    /* Register server: */
+    http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(req.url);
-    res.write();
+    htmlPage(req, res);
     console.log("Serving " + req.url);
     res.end();
 }).listen(8080);

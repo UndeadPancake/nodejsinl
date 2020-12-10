542c3a393d98f678a1883b2154fc5cec1fc46cdf
Martin Johansson <maxamartin1999@gmail.com>
2020-12-10 15:34:59 +0100

    Added compPage functionality

--- v50uppgifter/app.js
+++ v50uppgifter/app.js
@@ -1,5 +1,7 @@
 /* Includes: */
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
 
 function htmlPage(req, res) {
     res.write('<html>');
@@ -11,12 +13,49 @@ function htmlPage(req, res) {
             res.write('<p>My first paragraph</p>');
         res.write('</body>');
     res.write('</html>');
+    res.end();
+}
+
+function calcPage(req, res) {
+    fs.readFile('jscalc.html', function (err, data) {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        res.end();
+    });
+}
+
+function compPage(req, res, qdata) {
+    var result;
+    switch (qdata.op) {
+        case "plus":
+            result = parseInt(qdata.x) + parseInt(qdata.y);
+            res.write(qdata.x + " + " + qdata.y + " = " + result);
+            break;
+        case "minus":
+            result = qdata.x - qdata.y;
+            res.write(qdata.x + " - " + qdata.y + " = " + result);
+            break;
+        case "times":
+            result = qdata.x * qdata.y;
+            res.write(qdata.x + " * " + qdata.y + " = " + result);
+            break;
+    }
+    res.end();
 }
 
     /* Register server: */
-    http.createServer(function (req, res) {
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    htmlPage(req, res);
+    if (q.pathname == '/calc') {
+        calcPage(req, res);
+    }
+    else if (q.pathname == '/compute') {
+        var qdata = q.query;
+        compPage(req, res, qdata);
+    }
+    else {
+        htmlPage(req, res);
+    }
     console.log("Serving " + req.url);
-    res.end();
 }).listen(8080);
